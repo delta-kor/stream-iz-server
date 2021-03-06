ef04555ed78b1915634e357cb7577c3ac99a0839
delta-kor <deltakbot1@gmail.com>
2021-03-06 19:24:24 +0900

    Add mongoose, passport, session

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import MongoStore from 'connect-mongo';
+import express, { Application, json, NextFunction, Request, Response } from 'express';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import passport from 'passport';
 import HttpException from './exceptions/http.exception';
 import NotFoundException from './exceptions/not-found.exception';
 import Controller from './types/controller.class';
@@ -10,12 +14,40 @@ export default class App {
   constructor(port: number, controllers: Controller[]) {
     this.port = port;
     this.app = express();
+    this.connectDatabase();
+    this.mountMiddlewares();
     this.mountControllers(controllers);
     this.mountErrorHandling();
   }
 
   public listen(callback: (port: number) => void): void {
     this.app.listen(this.port, () => callback(this.port));
+  }
+
+  private connectDatabase(): void {
+    mongoose
+      .connect(process.env.DB_PATH!, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      })
+      .then(() => console.log('Connected to database'));
+  }
+
+  private mountMiddlewares(): void {
+    this.app.use(json());
+    this.app.use(
+      session({
+        name: 'icarus',
+        resave: true,
+        secret: process.env.SECRET!,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.DB_PATH! }),
+      })
+    );
+
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
   }
 
   private mountControllers(controllers: Controller[]): void {
