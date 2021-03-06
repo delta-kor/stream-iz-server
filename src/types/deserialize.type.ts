42ff0be3a1c9d28beea42f55ad1a4e7976e0744f
delta-kor <deltakbot1@gmail.com>
2021-03-06 20:35:27 +0900

    Fix deserializing

--- src/types/deserialize.type.ts
+++ src/types/deserialize.type.ts
@@ -1,9 +1,14 @@
 import { Document } from 'mongoose';
 
+type DateToNumber<T> = {
+  [K in keyof T]: T[K] extends Date ? number : T[K];
+};
 type NonFunctionPropertyNames<T> = {
   [K in keyof T]: T[K] extends Function ? never : K;
 }[keyof T];
 type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
 type Unextend<T, Q> = Pick<T, Exclude<keyof T, keyof Q>>;
 
-export type Deserialize<T extends Document> = Partial<NonFunctionProperties<Unextend<T, Document>>>;
+export type Deserialize<T extends Document> = Partial<
+  DateToNumber<NonFunctionProperties<Unextend<T, Document>>>
+>;
